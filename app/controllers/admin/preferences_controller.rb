e4b018f9d88afe04bb7007320d1152e3e040ed24
github username <github email address>
2018-10-15 15:33:10 -0400

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,28 @@
+class Admin::PreferencesController < ApplicationController
+    def index
+        @preference = Preference.new
+      end
+    
+      def create
+         @preference = Preference.new
+         if @preference.save
+          redirect_to preference
+        else
+        end
+      end
+    
+      def edit
+      end
+    
+      def update
+      end
+    
+      def destroy
+      end
+    
+      private 
+    
+      def preferences_params
+        params.require(:preference).permit(:song_order, :artist_order, :song_create_permission)
+      end
+end
